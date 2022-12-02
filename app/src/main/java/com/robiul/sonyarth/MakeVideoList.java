358fe62b7ef06d967283217acf17596674a836a4
Mahmodul5839 <developer5839@gmail.com>
2022-12-02 22:42:33 +0600

    commit two

---
+++ app/src/main/java/com/robiul/sonyarth/MakeVideoList.java
@@ -0,0 +1,129 @@
+
+package com.robiul.sonyarth;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MakeVideoList {
+
+	public static ArrayList< ArrayList<HashMap<String,String>> > rootArrayList;
+	public static ArrayList< HashMap<String, String> > catArrayList;
+	public static ArrayList< HashMap<String, String> > videoArrayList;
+	public static HashMap<String, String> hashMap;
+
+
+	//--------------------------------------------------------------------------------------------
+	//===============================Some automation by Juba
+	public static void addVideoItem(String video_id, String title, String desciption){
+		hashMap = new HashMap<>();
+		hashMap.put("vdo_id", video_id);
+		hashMap.put("vdo_title", title);
+		hashMap.put("vdo_desciption", desciption);
+		videoArrayList.add(hashMap);
+	}
+	//========================================================================
+	//===============================Some automation by Juba
+	public static void createPlayListForVideo(String category_name, Integer drawable){
+		rootArrayList.add(videoArrayList);
+		hashMap = new HashMap<>();
+		hashMap.put("category_name", category_name);
+		hashMap.put("img", String.valueOf(drawable));
+		catArrayList.add(hashMap);
+		videoArrayList = new ArrayList<>();
+	}
+	//========================================================================
+
+	//===============================Some automation by Juba
+	public static void createCategoryForWebsite(String category_name, Integer drawable, String url){
+		rootArrayList.add(videoArrayList);
+		hashMap = new HashMap<>();
+		hashMap.put("category_name", category_name);
+		hashMap.put("img", String.valueOf(drawable));
+		hashMap.put("url", url);
+		catArrayList.add(hashMap);
+		videoArrayList = new ArrayList<>();
+	}
+	//========================================================================
+
+	//===============================Some automation by Juba
+	public static void createCategoryForPDF(String category_name, Integer drawable, String pdfAssetName){
+		rootArrayList.add(videoArrayList);
+		hashMap = new HashMap<>();
+		hashMap.put("category_name", category_name);
+		hashMap.put("img", String.valueOf(drawable));
+		hashMap.put("pdfAssetName", pdfAssetName);
+		catArrayList.add(hashMap);
+		videoArrayList = new ArrayList<>();
+	}
+	//========================================================================
+
+
+
+
+
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//----------------------------------------------------
+	public static void createMyAlbums(){
+
+		rootArrayList = new ArrayList();
+		catArrayList = new ArrayList<>();
+		videoArrayList = new ArrayList<>();
+
+		/*
+		//========================Video Item Category
+		addVideoItem("GgaxcNmm4UU", "Ei Mom Jochonay | Madhubanti Mukherjee", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("qPna1ZlRxTo", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("hegsoEcow8Q", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		addVideoItem("kLHCDZeTVJk", "Allah di kasam tu mainu enna pyara ho gaya", "Teri har cheez jannat ae -hasna vi jannt ae");
+		createPlayListForVideo("Funny Dog", R.drawable.category_1);
+
+		//=========================Category for website loader
+		createCategoryForWebsite("Prothom Alo", R.drawable.category_2, "https://www.prothomalo.com/");
+
+		//=========================Category for website loader
+		createCategoryForWebsite("PDF FromUrl", R.drawable.category_3, "https://drive.google.com/file/d/1vB-oWIwJU6KuIKnIoqWukjZEyNjrd7IJ/view?usp=sharing");
+
+		//===========================Category for PDF Viewer
+		createCategoryForPDF("PDF FromAsset", R.drawable.category_4, "my_sample_pdf.pdf");
+		//==========================================================================*
+		 */
+
+
+		//========================Video Item Category
+
+		//=========================
+		createCategoryForWebsite("All Entertainment ", R.drawable.live_entertrainment, "https://www.bioscopelive.com/en/featured-tv");
+
+		createCategoryForWebsite("S.A Channel", R.drawable.logo_sony, "https://www.bioscopelive.com/en/channel/sony-aath-live");
+
+		createCategoryForWebsite("Animals TV", R.drawable.animalls, "https://www.bioscopelive.com/en/channel/bbc-earth-hd-live");
+
+
+		createCategoryForWebsite("cartoon TV Channel", R.drawable.cartoon1, "https://www.bioscopelive.com/en/channel/sony-yay-live");
+
+
+		createCategoryForWebsite("Cartoon TV Channel", R.drawable.cartoon2, "https://www.bioscopelive.com/en/channel/nickelodeon");
+
+
+
+
+
+
+
+	}
+
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+	//---------------------------------------------------->>>>>>
+
+
+
+
+}
+
